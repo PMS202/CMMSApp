5e222477465a6066de5270b835c777b71b8b6951
PHUC DUC HOANG NGUYEN <PHUC.DUCHOANG.NGUYEN@yageo.com>
2026-04-04 17:14:07 +0700

    Update 04.Apr: Problem report start

--- Other_code/export_excel.py
+++ Other_code/export_excel.py
@@ -9,10 +9,26 @@ from Database.MariaDB import Database_process
 
 db = Database_process()
 
-frame = db.query(sql = '''SELECT machine_name,machine_code, line_name,status FROM maintenance_with_status
-WHERE department_name = "PE3" COLLATE utf8mb4_unicode_ci AND status COLLATE utf8mb4_unicode_ci IN ("Near due", "Overdue") 
-ORDER BY line_name ASC, machine_code ASC;''')
+# frame = db.query(sql = '''SELECT m.machine_code,m.machine_name,pl.line_name, d.department_name
+#                             FROM machines m
+#                             JOIN maintenance_plan mp 
+#                                 ON m.machine_id = mp.machine_id
+#                             JOIN production_lines pl 
+#                                 ON mp.line_id = pl.line_id
+#                             JOIN departments d 
+#                                 ON pl.department_id = d.department_id
+#                             WHERE mp.maintenance_date IS NULL
+#                             AND ( mp.status IS NULL OR mp.status = '') AND d.department_name = 'PE3' AND mp.month_year_id = 15;''')
 
-df = pd.DataFrame(frame, columns=['machine_name', 'machine_code', 'line_name', 'status'])
-output_path = os.path.join(PROJECT_ROOT, 'exported_files', 'PE3_MAR.xlsx')
+# frame = db.query(sql = '''SELECT machine_code,machine_name,line_name,department_name
+#                             FROM maintenance_with_status
+#                             WHERE department_name = 'PE3' COLLATE utf8mb4_unicode_ci  AND status = "Near due" COLLATE utf8mb4_unicode_ci;''')
+
+
+frame = db.query(sql = '''SELECT machine_code,machine_name,line_name,department_name
+                            FROM view_record_pending
+                            WHERE department_name = 'PE3';''')
+
+df = pd.DataFrame(frame, columns=['machine_code', 'machine_name', 'line_name', 'department_name'])
+output_path = os.path.join(PROJECT_ROOT, 'exported_files', 'PE3_PENDING_MAR.xlsx')
 df.to_excel(output_path, index=False)
